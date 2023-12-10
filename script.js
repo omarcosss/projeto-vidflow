fd3874f51d4ea4fd54c607e70cb647cd1882793a
omarcosss <89608993+omarcosss@users.noreply.github.com>
2023-12-10 09:49:59 -0300

    final

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 const containerVideos = document.querySelector(".videos__container");
 
 
+async function buscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+        
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem} alt="logo do canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
+                </div>
+            </li>
+            `;
+        })
+    } catch(error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos.</p>`
+    }
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) => 
+}
+
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+
     videos.forEach((video) => {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem} alt="logo do canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
+        const titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
 
-)
-.catch((error)=> {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error} </p>`
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? "block" : "none" : "block";
+    })
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
 })
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        //video.style.display = categoria.includes(valorFiltro) && valorFiltro != 'tudo' ? "block" : "none";
+        if (!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = "none"
+        } else {
+            video.style.display = "block"
+        }
+    }
+}
